84f9d314658260e5655c6769ad408141df9f458e
luakso <38975851+luakso@users.noreply.github.com>
2026-05-26 09:06:49 +0800

    chore: drop internal/deps.go anchor (deps re-promote naturally in Tasks 2+)

--- internal/deps.go
+++
@@ -1,8 +0,0 @@
-// Package internal provides internal utilities for the fathom service.
-package internal
-
-import (
-	_ "github.com/ethereum/go-ethereum/crypto" // Used for keccak256 hashing
-	_ "github.com/shopspring/decimal"          // Used for NUMERIC(78,0) database roundtrips with pgx
-	_ "github.com/stretchr/testify/require"    // Used for test assertions
-)
